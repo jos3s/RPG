961dbe9223569982946dc1e88251f94e9c643d78
jos3s <karatedkgt17@gmail.com>
2019-12-06 22:31:53 -0300

    Função habilidades adicionada

--- rpg.h
+++ rpg.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct personagem{
+typedef struct personagem{      //Struct que cria a variave PL que serve como base para o personagem
     char nome[20];
     int vida;
     int defesa;
@@ -32,7 +32,7 @@ int combo(int d, int v){
 PL criar(PL j){
     int a=0;
     j.vida=1000;
-    do{     //personagem 1
+    do{     //cAtribuição dos pontos dos personagens
         puts("Você possui 60 pontos defina a categoria deles.");
         printf("Pontos de defesa:");
         scanf("%d", &j.defesa);
@@ -45,6 +45,21 @@ PL criar(PL j){
             puts("Você ultrapassou o limite permitido, repita novamente");
     }while(a>60);
 return j;
+}
+
+void habilidades(){         //Visualize as informações de habilidades dos personagens
+    int h;
+    printf("\nDeseja ver a suas habilidades:\n Digite 1 para visualizar, 2 para não exibir. ");
+    scanf("%d", &h);
+    if(h==1){
+        puts("\nHabilidades");
+        printf("Nome:\t%s \t %s\n", j1.nome, j2.nome);
+        printf("Vida:\t%d \t %d\n", j1.vida,j2.vida);
+        printf("Defesa:\t%d \t %d\n", j1.defesa,j2.defesa);
+        printf("Força:\t%d \t %d\n", j1.forca, j2.forca);
+        printf("Vel.:\t%d \t %d\n", j1.velocidade, j2.velocidade);
+        puts("");
+    }
 }
 
 #endif
