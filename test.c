24f31612663d04d61abad6ae51389c61bea781b7
jos3s <karatedkgt17@gmail.com>
2019-12-07 23:13:01 -0300

    Correção/Funções e linhas inúteis

--- test.c
+++ test.c
@@ -6,6 +6,46 @@
 
 PL j1,j2;
 
+int jogador1(PL j1, PL j2){
+    int dano;
+    int at, es, com, def;
+    int esc,dado;
+    char p,pt;
+
+    printf("%s vai ser o primeiro", j1.nome);
+    do{
+        printf("\n\nJogador %s vai atacar\n", j1.nome);
+        printf("Jogador %s escolha 1 para esquivar ou 2 para defender: ",j2.nome);
+        scanf("%d", &esc);
+        if(esc==1){
+            j2.vida-=jesquivar(j1.forca, j1.velocidade, j2.velocidade);
+            printf("\nA vida atual de %s é de: %d",j2.nome, j2.vida);
+            if(j2.vida<=0)
+                break;
+        }else{
+            j2.vida-=jdefesa(j1.forca,j1.velocidade,j2.defesa);
+            printf("\nA vida atual de %s é de: %d",j2.nome, j2.vida);
+            if(j2.vida<=0)
+                break;
+        }
+        printf("\n\nJogador %s vai atacar",j2.nome);
+        printf("\nJogador %s escolha 1 para esquivar ou 2 para defender: ",j1.nome);
+        scanf("%d", &esc);
+        if(esc==1){
+            j1.vida-=jesquivar(j2.forca, j2.velocidade, j1.velocidade);
+            printf("\nA vida atual de %s é de: %d",j1.nome, j1.vida);
+            if(j1.vida<=0)
+                break;
+        }else{
+            j1.vida-=jdefesa(j2.forca,j2.velocidade,j1.defesa);
+            printf("\nA vida atual de %s é de: %d",j1.nome, j1.vida);
+            if(j1.vida<=0)
+                break;
+        }
+    }while(j1.vida>0 || j2.vida>0);
+        return 0;
+}
+
 int jogador2(PL j1, PL j2){    
     int dano;
     int at, es, com, def;
@@ -17,83 +57,37 @@ int jogador2(PL j1, PL j2){
         printf("\n\nJogador %s vai atacar", j2.nome);
         printf("\nJogador %s escolha 1 para esquivar ou 2 para defender: ",j1.nome);
         scanf("%d", &esc);
-            if(esc==1){
-                j1.vida-=jesquivar(j2.forca, j2.velocidade, j1.velocidade);
-                printf("\nA vida atual de %s é de: %d",j1.nome, j1.vida);
-                if(j1.vida<=0)
-                    break;
-            }else{
-                j1.vida-=jdefesa(j2.forca,j2.velocidade,j1.defesa);
-                printf("\nA vida atual de %s é de: %d",j1.nome, j1.vida);
-                if(j1.vida<=0)
-                    break;
-            }
-            printf("\n\nJogador %s vai atacar\n",j1.nome);
-            printf("Jogador %s escolha 1 para esquivar ou 2 para defender: ",j2.nome);
-            scanf("%d", &esc);
-            if(esc==1){
-                j2.vida-=jesquivar(j1.forca, j1.velocidade, j2.velocidade);
-                printf("\nA vida atual de %s é de: %d",j2.nome, j2.vida);
-                if(j2.vida<=0)
-                    break;
-            }else{
-                j2.vida-=jdefesa(j1.forca,j1.velocidade,j2.defesa);
-                printf("\nA vida atual de %s é de: %d",j2.nome, j2.vida);
-                if(j2.vida<=0)
-                    break;
-            }
-        }while(j1.vida>0 || j2.vida>0);
-        return 0;
-}
-
-int jogador1(PL j1, PL j2){
-    int dano;
-    int at, es, com, def;
-    int esc,dado;
-    char p,pt;
-
-    printf("%s vai ser o primeiro", j1.nome);
-    do{
-        printf("\n\nJogador %s ataca\n", j1.nome);
+        if(esc==1){
+            j1.vida-=jesquivar(j2.forca, j2.velocidade, j1.velocidade);
+            printf("\nA vida atual de %s é de: %d",j1.nome, j1.vida);
+            if(j1.vida<=0)
+                break;
+        }else{
+            j1.vida-=jdefesa(j2.forca,j2.velocidade,j1.defesa);
+            printf("\nA vida atual de %s é de: %d",j1.nome, j1.vida);
+            if(j1.vida<=0)
+                break;
+        }
+        printf("\n\nJogador %s vai atacar\n",j1.nome);
         printf("Jogador %s escolha 1 para esquivar ou 2 para defender: ",j2.nome);
         scanf("%d", &esc);
-            if(esc==1){
-                j2.vida-=jesquivar(j1.forca, j1.velocidade, j2.velocidade);
-                printf("\nA vida atual de %s é de: %d",j2.nome, j2.vida);
-                if(j2.vida<=0)
-                    break;
-            }else{
-                j2.vida-=jdefesa(j1.forca,j1.velocidade,j2.defesa);
-                printf("\nA vida atual de %s é de: %d",j2.nome, j2.vida);
-                if(j2.vida<=0)
-                    break;
-            }
-            printf("\n\nJogador %s vai atacar",j2.nome);
-            printf("\nJogador %s escolha 1 para esquivar ou 2 para defender: ",j1.nome);
-            scanf("%d", &esc);
-            if(esc==1){
-                j1.vida-=jesquivar(j2.forca, j2.velocidade, j1.velocidade);
-                printf("\nA vida atual de %s é de: %d",j1.nome, j1.vida);
-                if(j1.vida<=0)
-                    break;
-            }else{
-                j1.vida-=jdefesa(j2.forca,j2.velocidade,j1.defesa);
-                printf("\nA vida atual de %s é de: %d",j1.nome, j1.vida);
-                if(j1.vida<=0)
-                    break;
-            }
+        if(esc==1){
+            j2.vida-=jesquivar(j1.forca, j1.velocidade, j2.velocidade);
+            printf("\nA vida atual de %s é de: %d",j2.nome, j2.vida);
+            if(j2.vida<=0)
+                break;
+        }else{
+            j2.vida-=jdefesa(j1.forca,j1.velocidade,j2.defesa);
+            printf("\nA vida atual de %s é de: %d",j2.nome, j2.vida);
+            if(j2.vida<=0)
+                break;
+        }
     }while(j1.vida>0 || j2.vida>0);
         return 0;
 }
 
 void jogadores(){
-    puts("\nJogador 1");
-    printf("Digite o nome do seu personagem:");
-    scanf(" %19[^\n]s", &j1.nome);
     j1=criar(j1);
-    puts("\nJogador 2");
-    printf("Digite o nome do seu personagem:");
-    scanf(" %19[^\n]s", &j2.nome);
     j2=criar(j2);
 }
 
